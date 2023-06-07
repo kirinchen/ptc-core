beaa40ec3e4994db8d6a16e20bac4ac57c1f79b9
kirin <kirin.chen1001@gmail.com>
2023-06-07 09:33:17 +0800

    init commit

---
+++ ptc_core/src/main/java/com/domi/ptc_core/PtcCoreApplication.java
@@ -0,0 +1,13 @@
+package com.domi.ptc_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtcCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtcCoreApplication.class, args);
+	}
+
+}
---
+++ ptc_core/src/test/java/com/domi/ptc_core/PtcCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domi.ptc_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtcCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
